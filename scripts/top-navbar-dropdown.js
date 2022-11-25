1037af74d5f57ffdab7b869df7e7b1c24473404f
Prof. Otávio Medeiros <prof.otavio.medeiros@gmail.com>
2022-11-25 16:16:24 -0300

    main > commit 001 > 2022-11-25 16:16:24 -03:00:00

---
+++ scripts/top-navbar-dropdown.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  let myTopnav = document.getElementById("myTopnav");
+  if (myTopnav.className === "topnav") {
+    myTopnav.className += " responsive";
+  } else {
+    myTopnav.className = "topnav";
+  }
+}
